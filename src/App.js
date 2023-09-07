6547a8ee57fb34a015ec98fd7c6fff7f121aa0ae
mariagiohanna <mgmartinez@fiuna.edu.py>
2023-09-06 21:03:53 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ function App() {
       <header className="App-header">
         <h1>Weather App</h1>
         <WeatherForm />
+        <p>
+          Coded by Giohanna. OpenSource 👉
+          <a href="https://github.com/mariagiohanna/weather-app-react">
+            Tap here
+          </a>
+        </p>
       </header>
     </div>
   );
